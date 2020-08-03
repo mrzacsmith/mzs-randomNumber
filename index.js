d95acbf49a7412889cc2a9bd37d7b398ab6c4f9f
Zac Smith <zrs3141592@gmail.com>
2020-08-03 11:17:51 -0600

    create random number generator

---
+++ index.js
@@ -0,0 +1,9 @@
+const randomNumGenerator = (min, max) => {
+  if (typeof max !== 'number' && typeof min !== 'number') {
+    min = 0
+    max = 1
+  }
+  return Math.random() * (max - min) + min
+}
+
+module.exports = randomNumGenerator
